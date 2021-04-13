e7061715321b7e8436af7b2424d6fa6b4785dd37
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-04-13 14:56:58 +0600

    route added

---
+++ route/Route.php
@@ -0,0 +1,26 @@
+<?php
+class Route
+{
+    const pages_array = array(
+        'index' => array(
+            'slug' => 'index.php',
+            'title' => 'Home',
+        ),
+        'about' => array(
+            'slug' => 'about.php',
+            'title' => 'About',
+        ),
+        'login' => array(
+            'slug' => 'login.php',
+            'title' => 'Login',
+        ),
+        'register' => array(
+            'slug' => 'register.php',
+            'title' => 'Register',
+        ),
+        'dashboard' => array(
+            'slug' => 'dashboard.php',
+            'title' => 'Dashboard',
+        ),
+    );
+}
